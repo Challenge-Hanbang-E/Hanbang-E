f956c53250246fa4944c29c698c2511b5e7464a7
mina <m1nna@naver.com>
2022-12-31 14:56:34 +0900

    init

---
+++ src/main/java/com/hanbang/e/HanbangEApplication.java
@@ -0,0 +1,13 @@
+package com.hanbang.e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbangEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanbangEApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanbang/e/HanbangEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbang.e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbangEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
