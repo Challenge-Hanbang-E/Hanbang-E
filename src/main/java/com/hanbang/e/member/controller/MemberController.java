3167e76b83a1f15378136ce5372ee286be5957dc
seungit <minsg3669@gmail.com>
2023-01-09 16:33:09 +0900

    [feat] 로그인 기능 추가. 1. 로그인 기능 추가
    Resolves: #9

--- src/main/java/com/hanbang/e/member/controller/MemberController.java
+++ src/main/java/com/hanbang/e/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.hanbang.e.member.controller;
 
 import com.hanbang.e.common.dto.ResponseDto;
 import com.hanbang.e.member.dto.MemberCreateReq;
+import com.hanbang.e.member.dto.MemberLoginReq;
 import com.hanbang.e.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -25,5 +28,12 @@ public class MemberController {
     public ResponseEntity<?> signup(@RequestBody @Valid MemberCreateReq memberCreateReq) {
         memberService.signup(memberCreateReq);
         return new ResponseEntity<>(new ResponseDto<>("success", "회원가입 완료", null), HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody MemberLoginReq memberLoginReq, HttpServletResponse httpServletResponse) {
+        memberService.login(memberLoginReq, httpServletResponse);
+        return new ResponseEntity<>(new ResponseDto<>("success","로그인 완료", null),HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/hanbang/e/member/dto/MemberLoginReq.java
@@ -0,0 +1,24 @@
+package com.hanbang.e.member.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class MemberLoginReq {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식을 맞춰주세요.")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!#%*?&])[A-Za-z\\d@$!#%*?&]{8,15}$",
+            message = "비밀번호는 8~15자리의 대소문자,숫자,특수문자로 이루어져야 합니다.")
+    private String password;
+
+    public MemberLoginReq(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
