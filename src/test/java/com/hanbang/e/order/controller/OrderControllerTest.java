1d987dfa0a310955146106faeebc2e021bffec4e
Hyun <itsmine096@gmail.com>
2023-01-09 12:46:41 +0900

    [test] 상품 주문 Controller 계층 테스트 코드 작성
    1. 테스트 코드에 필요한 라이브러리 의존성 추가
    2. 상품 주문 기능 컨트롤러 계층 테스트 코드 추가
    
    Related to: #5

---
+++ src/test/java/com/hanbang/e/order/controller/OrderControllerTest.java
@@ -0,0 +1,81 @@
+package com.hanbang.e.order.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanbang.e.member.entity.Member;
+import com.hanbang.e.member.repository.MemberRepository;
+import com.hanbang.e.order.dto.OrderReq;
+import com.hanbang.e.product.entity.Product;
+import com.hanbang.e.product.repository.ProductRepository;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OrderControllerTest {
+
+	@Autowired
+	TestRestTemplate rt;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private static ObjectMapper om;
+	private static HttpHeaders headers;
+
+
+	@BeforeAll
+	public static void init() {
+		om = new ObjectMapper();
+		headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+	}
+
+	@DisplayName("상품 주문")
+	@Test
+	void doOrderTest() throws JsonProcessingException {
+		/* given - 데이터 준비 */
+		Member dummyMember = new Member("tj087@naver.com", "1234", "제주시");
+		Product product1 = Product.builder()
+			.productName("아이폰11")
+			.price(500000L)
+			.img("http....")
+			.stock(10)
+			.sales(50)
+			.onSale(true)
+			.build();
+		memberRepository.save(dummyMember);
+		productRepository.save(product1);
+
+		OrderReq order = new OrderReq(1);
+		String body = om.writeValueAsString(order);
+
+		/* when - 테스트 실행 */
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+		ResponseEntity<String> response = rt.exchange("/api/order?productId=1", HttpMethod.POST, request, String.class);
+
+		/* then - 검증 */
+		DocumentContext dc = JsonPath.parse(response.getBody());
+		String result = dc.read("$.result");
+		String msg = dc.read("$.msg");
+
+		assertThat(result).isEqualTo("success");
+		assertThat(msg).isEqualTo("주문 성공");
+	}
+}
